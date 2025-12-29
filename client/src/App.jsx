1e01c1254171f38c807907c6aba7f3f62d12d7ff
Aditi Inamdar <66082809+aditiinamdar@users.noreply.github.com>
2025-12-29 11:38:49 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,9 +15,9 @@ import AddAddress from './pages/AddAddress.jsx';
 import SellerLogin from "./components/seller/SellerLogin";
 
 import Orders from "./pages/seller/Orders.jsx";
-import SellerLayout from './pages/Seller/SellerLayout.jsx';
-import AddProduct from './pages/Seller/AddProduct.jsx';
-import ProductList from './pages/Seller/ProductList.jsx';
+import SellerLayout from './pages/seller/SellerLayout.jsx';
+import AddProduct from './pages/seller/AddProduct.jsx';
+import ProductList from './pages/seller/ProductList.jsx';
 
 
 
